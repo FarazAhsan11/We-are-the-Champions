3c7e508e3e011c88d9b25ac83af0459fecad0ac8
Muhammad Faraz Ahsan <127343300+FarazAhsan11@users.noreply.github.com>
2025-10-23 12:49:52 +0500

    Remove database URL from app settings
    Clear the database URL in app settings.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 const appSettings = {
-    databaseURL: "https://champions-b6b11-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: ""
 }
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
